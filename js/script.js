5e6978d35b6d502ed2bd80dda87740860c8e4cd4
lorenzoguerra01 <guerra.lo11-lg@gmail.com>
2024-03-27 13:18:03 +0100

    add html, css, js, vue

---
+++ js/script.js
@@ -0,0 +1,13 @@
+import {} from './utility.js';
+
+const {createApp} = Vue;
+createApp({
+    data(){
+        return{
+            message : 'Hello world!!!'
+        }
+    },
+    methods :{
+
+    }
+}).mount('#app');
